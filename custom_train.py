914250b01bf74890b49f0f0a755f51f2782953e7
Luca Scionis <lucascionis@tiscali.it>
2023-08-30 12:51:41 +0200

    Update custom_train.py
    - modified training with gallery images

--- custom_train.py
+++ custom_train.py
@@ -2,12 +2,12 @@ import argparse
 import os
 import glob
 import pickle
+import copy
 
 import torch
 import torchreid
 from torchreid import models, metrics
 from torchreid.losses import CrossEntropyLoss, DeepSupervision
-# import torch.nn as nn
 
 import models
 
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description='adversarial attack')
 parser.add_argument('--target_model', type=str, default='aligned')
 parser.add_argument('--pre_dir', type=str, default='./models', help='path to the model')
 parser.add_argument('--queries_dir', type=str, default='./queries', help='path to be attacked model')
-parser.add_argument('--test_batch', default=32, type=int, help="test batch size")
+parser.add_argument('--train_batch', default=10, type=int, help="train batch size")
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -36,33 +36,23 @@ def check_freezen(net, need_modified=False, after_modified=None):
             # else: print('child', cc , 'was frozen')
         cc += 1
 
-
-def parse_data_for_train(data):
-    imgs = data[0]
-    pids = data[1]
-    return imgs, pids
-
-
-def compute_loss(criterion, outputs, targets):
-    if isinstance(outputs, (tuple, list)):
-        loss = DeepSupervision(criterion, outputs, targets)
-    else:
-        loss = criterion(outputs, targets)
+def DeepSupervision(criterion, xs, labels):
+    loss = 0.
+    for x in xs:
+      loss += criterion(x, labels)
     return loss
 
 
-def train_model(model, data, optimizer, criterion):
-    imgs, pids = parse_data_for_train(data)
-
+def train_model(model, data, labels, optimizer, criterion):
+    imgs, _, _, _ = data
     imgs = imgs.to(device)
-    pids = pids.to(device)
+    labels = torch.tensor(labels, dtype=torch.int64).to(device)
 
     ls = model(imgs, is_training=True)
     if len(ls) == 1: outputs = ls[0]
     if len(ls) == 2: outputs, features = ls
     if len(ls) == 3: outputs, features, local_features = ls
-    loss = compute_loss(criterion, outputs, pids)
-    # loss = criterion(outputs, pids)
+    loss = DeepSupervision(criterion, outputs, labels)
 
     optimizer.zero_grad()
     loss.backward()
@@ -70,7 +60,7 @@ def train_model(model, data, optimizer, criterion):
 
     loss_summary = {
         'loss': loss.item(),
-        'acc': metrics.accuracy(outputs, pids)[0].item()
+        'acc': metrics.accuracy(outputs, labels)[0].item()
     }
 
     return loss_summary
@@ -81,26 +71,28 @@ def main():
     model = args.target_model
     weight_path = args.pre_dir
     queries_path = args.queries_dir
-    test_batch = args.test_batch
-    dataset = 'market1501'
+    train_batch = args.train_batch
+    dataset_name = 'market1501'
 
     opt = get_opts(model)
 
-    # load market151 dataset
-    dataset = data_manager.init_img_dataset(root='data', name=dataset, split_id=opt['split_id'],
+    dataset = data_manager.init_img_dataset(root='data', name=dataset_name, split_id=opt['split_id'],
                                             cuhk03_labeled=opt['cuhk03_labeled'],
                                             cuhk03_classic_split=opt['cuhk03_classic_split'])
 
-    '''
-    galleryloader = DataLoader(ImageDataset(dataset.gallery, transform=opt['transform_test']),
-                               batch_size=test_batch, shuffle=False, num_workers=opt['workers'],
-                               pin_memory=torch.cuda.is_available(), drop_last=False)
-    '''
+    # for each query load the indices of the top-k predictions
+    queries_idxs = []
+    q_path = os.path.join(queries_path, '**/*.pkl')
+    for query_idxs in glob.glob(q_path, recursive=True):
+        with open(query_idxs, 'rb') as file:
+            queries_idxs.append(pickle.load(file))
 
-    # load pretraind model
-    # model = models.build_model(name=model, num_classes=dataset.num_train_pids)
-    # torchreid.utils.load_pretrained_weights(model, weight_path)
     target_net = models.init_model(name=model, pre_dir=weight_path, num_classes=dataset.num_train_pids)
+
+    _target_net_cl = copy.deepcopy(target_net.classifier_local)
+    target_net.classifier_local = torch.nn.Linear(in_features=512, out_features=2, bias=True)
+    # print(target_net)
+
     # check_freezen(target_net, need_modified=True, after_modified=True)
     target_net.to(device)
     target_net.train()
@@ -111,43 +103,44 @@ def main():
         lr=0.0003
     )
 
-    scheduler = torchreid.optim.build_lr_scheduler(
-        optimizer,
-        lr_scheduler="single_step",
-        stepsize=20
-    )
-
-    criterion = CrossEntropyLoss(
-        num_classes=dataset.num_train_pids,
+    criterion = torchreid.losses.CrossEntropyLoss(
+        num_classes=2,
         use_gpu=torch.cuda.is_available,
-        label_smooth=True
+        label_smooth=False
     )
 
-    # for each query load the indices of the top-k predictions
-    queries_idxs = []
-    q_path = os.path.join(queries_path, '**/*.pkl')
-    for query_idxs in glob.glob(q_path, recursive=True):
-        with open(query_idxs, 'rb') as file:
-            queries_idxs.append(pickle.load(file))
-
+    # Prepare the gallery loaders
+    print("Preparing gallery loaders...")
     gallery_loaders = []
     for query_idxs in queries_idxs:
         # each query_idxs is a numpy array with indices from the gallery
         # create a subset of dataset galley using the indices
-        g_subset = Subset(dataset.gallery, query_idxs)
+        idxs = torch.tensor(query_idxs[:, 0], dtype=int)
+        labels = query_idxs[:, 1]
+        g_subset = Subset(dataset.gallery, idxs)
         # create a dataloader using the subset
-        # g_loader = DataLoader(g_subset, batch_size=1, shuffle=False, num_workers=2)
         g_loader = DataLoader(ImageDataset(g_subset, transform=opt['transform_train']),
-                               batch_size=5, shuffle=False, num_workers=opt['workers'],
-                               drop_last=False)
-        gallery_loaders.append(g_loader)
+                              batch_size=train_batch, shuffle=False, num_workers=opt['workers'],
+                              drop_last=False)
+        gallery_loaders.append([g_loader, labels])
 
-    for g_loader in gallery_loaders:
-        for idx, batch in enumerate(g_loader):
-            loss_summary = train_model(target_net, batch, optimizer, criterion)
+    # Train the model using each gallery loader data
+    print("Training re-id model with gallery images...")
+    for g_l in gallery_loaders:
+        loader = g_l[0]
+        labels = g_l[1]
+
+        for idx, batch in enumerate(loader):
+            labels_batch = labels[idx * train_batch:(idx + 1) * train_batch]
+            loss_summary = train_model(target_net, batch, labels_batch, optimizer, criterion)
             print(loss_summary)
-        # eventually save this as model_best_queryID.pth
 
+    # Saving the trained model
+    print("Saving trained model...")
+    target_net.classifier_local = _target_net_cl
+
+    file_name = 'retrained_{}'.format(model)
+    torch.save(target_net.state_dict(), '/content/drive/MyDrive/adv_reid/retrained/{}.pth.tar'.format(file_name))
 
 if __name__ == '__main__':
     main()
